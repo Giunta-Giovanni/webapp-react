0ad74158af6353fea69f0802ead263e79c01e2ef
GiuntaGiovanni <jhonniest741@gmail.com>
2025-03-11 16:03:06 +0100

    [feat] create router connection with HomePage & moviePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
+// importiamo la parte di gestione delle rotte da modulo react-router
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// importiamo il layout
+import DefaultLayout from './layout/DefaultLayout';
+
+// importiamo le pagine
+import HomePage from "./pages/HomePage";
+import MoviePage from "./pages/MoviePage";
+
+
 export default function App() {
   return (
-    <>
-      <h1>ciao a tutti</h1></>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DefaultLayout />}>
+          <Route index path="/" element={<HomePage />} />
+          <Route path="/Movie/:id" element={<MoviePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
