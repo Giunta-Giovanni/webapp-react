21faed52848d65fb1536f20b67dc3659cc9d3e3a
GiuntaGiovanni <jhonniest741@gmail.com>
2025-03-11 18:05:26 +0100

    [feat] add axios call in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,21 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import MovieCard from "../components/MovieCard"
 export default function HomePage() {
+    function fetchMovies() {
+        axios.get('http://localhost:3000/api/movies')
+            .then(res => {
+                console.log(res.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    useEffect(() => {
+        fetchMovies();
+    }, []);
+
     return (
 
         <section className="card-section">
@@ -11,12 +27,6 @@ export default function HomePage() {
             <MovieCard />
             <MovieCard />
             <MovieCard />
-
-
-
-
-
-
         </section>
 
     )
