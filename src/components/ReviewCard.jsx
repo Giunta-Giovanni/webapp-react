3d95e2b57b1da098e4a6d6523b634b18f5991baa
GiuntaGiovanni <jhonniest741@gmail.com>
2025-03-12 20:41:11 +0100

    [feat] css style in film section

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,7 +1,7 @@
 export default function ReviewCard({ reviewProp }) {
     const { created_at, id, movie_id, name, text, vote } = reviewProp
     return (
-        <div className="card mb-4">
+        <div className="card mb-4 bg-black">
             <div className="card-body">
                 <p className="card-text">
                     {text}
--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 import { useState, useEffect } from "react"
-import { useParams, useNavigate } from "react-router-dom"
+import { useParams, useNavigate, Link } from "react-router-dom"
 import ReviewCard from "../components/ReviewCard"
 export default function MoviePage() {
 
@@ -51,37 +51,59 @@ export default function MoviePage() {
         <>
             {/* sezione del film */}
             <section id="movie-details">
-                <div className="row">
-                    <div className="col-4 movie-cover">
-                        <img
-                            src={movie.image}
-                            alt={movie.title}
-                        />
-                    </div>
-                    <div className="col-8">
-                        <h1>{movie.title}</h1>
-                        <address><i>{movie.director}</i></address>
-                        <span>{movie.genre} </span>
-                        <span>{movie.release_year}</span>
+                <div className="movie-cover">
+                    <img
+                        src={movie.bg_image}
+                        alt={movie.title}
+                    />
+                </div>
+
+                <div className="movie-link">
+                    <Link>CONSIGLIATI</Link>
+                    <Link>EXTRA</Link>
+                    <Link className="active">DETTAGLI</Link>
+                </div>
+
+                {/* row contenete tutte le informazioni*/}
+                <div className="row mt-4">
+
+                    {/* col 1: titolo & abstract */}
+                    <div className="col-6">
+                        <h2>{movie.title}</h2>
                         <p>{movie.abstract}</p>
+                    </div>
+
+                    {/* col 2: genere, data di rilascio */}
+                    <div className="col-3">
+                        <h6>Genere:</h6>
+                        <p>{movie.genre} </p>
+                        <h6>Data di uscita:</h6>
+                        <p>{movie.release_year}</p>
+                    </div>
+
+                    {/* col 3: director */}
+                    <div className="col-3">
+                        <h6>Cast:</h6>
+                        <address><i>{movie.director}</i></address>
                     </div>
                 </div>
             </section >
 
             {/* sezione delle recensioni */}
-            <section id="reviews">
+            < section id="reviews" >
                 {/* parte superiore */}
-                <div className="d-flex justify-content-between align-items my-3">
+                < div className="d-flex justify-content-between align-items my-3" >
                     {/* parte sinistra */}
-                    <h5>Le nostre recensioni</h5>
+                    < h5 > Le nostre recensioni</h5 >
                     {/* parte destra */}
-                    <p>Average: star</p>
-                </div>
+                    < p > Average: star</p >
+                </div >
                 {/* parte inferiore */}
                 {renderReviews()}
-            </section>
+            </section >
 
-            <section className="card" id="new-review" >
+            {/* sezione nuova recensione */}
+            < section className="card bg-black" id="new-review" >
                 <div className="card-header">
                     Inserisci la tua recensione
                 </div>
