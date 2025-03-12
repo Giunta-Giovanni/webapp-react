453dacaef474585a52b443369e6c96d84bca2b89
GiuntaGiovanni <jhonniest741@gmail.com>
2025-03-12 20:27:51 +0100

    [feat] css style on header,home page & footer

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,25 +5,29 @@ export default function MovieCard(props) {
     const { id, title, director, abstract, image } = props.movieProp
 
     return (
-
         <div className="card">
-            <img
-                src={image}
-                alt={title}
-                className="card-img-top"
+            <Link to={`movie/${id}`}>
+                <div className="card-image">
+                    <img
+                        src={image}
+                        alt={title}
+                        className="card-img-top"
+                    />
+                </div>
 
-            />
-            <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <address><i>{director}</i></address>
-                <p className="card-text">{abstract}</p>
-                <Link
-                    className="btn btn-primary"
-                    to={`movie/${id}`}
-                >Vai al dettaglio
-                </Link>
-            </div>
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <address><i>{director}</i></address>
+                    <p className="card-text">{abstract}</p>
+                </div>
+                {/* square superiore con numero*/}
+                <div className="card-ranking">
+                    <span>{id}</span>
+                </div>
+            </Link >
+
         </div>
+
     )
 
 }
