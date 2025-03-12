e99e8a4017a7787acaf7ead62d715349e17ae043
GiuntaGiovanni <jhonniest741@gmail.com>
2025-03-12 15:18:44 +0100

    [feat] fetch movie details & render reviews 	•	Fetch movie details using useEffect. 	•	Display all necessary information on the movie page. 	•	Pass reviews as props to ReviewCard to render specific details.

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,15 +1,16 @@
-export default function ReviewCard() {
+export default function ReviewCard({ reviewProp }) {
+    const { created_at, id, movie_id, name, text, vote } = reviewProp
     return (
         <div className="card mb-4">
             <div className="card-body">
                 <p className="card-text">
-                    testo della recensione
+                    {text}
                 </p>
                 <strong>
-                    Vote:voto recensione
+                    Vote:{vote}
                 </strong>
                 <address>
-                    <i>By nome del recensore</i>
+                    <i>By {name}</i>
                 </address>
             </div>
         </div>
--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,25 +1,75 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
+import { useParams, useNavigate } from "react-router-dom"
 import ReviewCard from "../components/ReviewCard"
 export default function MoviePage() {
+
+    // requperiamo l'id
+    const { id } = useParams();
+
+    // utilizzo per il redirect di pagina (useNavigate)
+    const redirect = useNavigate();
+
+    // salviamo la variabile di stato del dettaglio
+    const [movie, setMovie] = useState({});
+
+    // funzione di chiamata all'api per il singolo film
+    function fetchMovie() {
+        axios.get(axios.get(`http://localhost:3000/api/movies/${id}`)
+            .then(res => {
+                setMovie(res.data)
+            })
+            .catch(err => {
+                console.log(err);
+                if (err.status === 404) redirect("/PageNotFound")
+            }))
+    }
+
+    // all'avvio della pagina richiama il movie
+    useEffect(fetchMovie, []);
+
+    useEffect(() => {
+        if (movie.length !== 0) {
+            console.log(movie)
+        }
+    }, [movie]);
+
+
+
+    function renderReviews() {
+        return movie.reviews?.map(
+            review =>
+                <ReviewCard
+                    key={review.id}
+                    reviewProp={review}
+                />
+        )
+    }
+
+
     return (
         <>
             {/* sezione del film */}
             <section id="movie-details">
                 <div className="row">
                     <div className="col-4 movie-cover">
-                        <img src="http://localhost:3000/img/movies_cover/inception.jpg" alt="" />
+                        <img
+                            src={movie.image}
+                            alt={movie.title}
+                        />
                     </div>
                     <div className="col-8">
-                        <h1>Titolo del Film</h1>
-                        <address><i>nome director</i></address>
-                        <span>genere </span>
-                        <span>anno di uscita</span>
-                        <p>abstract del film con continuazione...</p>
+                        <h1>{movie.title}</h1>
+                        <address><i>{movie.director}</i></address>
+                        <span>{movie.genre} </span>
+                        <span>{movie.release_year}</span>
+                        <p>{movie.abstract}</p>
                     </div>
                 </div>
             </section >
+
             {/* sezione delle recensioni */}
             <section id="reviews">
-
                 {/* parte superiore */}
                 <div className="d-flex justify-content-between align-items my-3">
                     {/* parte sinistra */}
@@ -27,13 +77,8 @@ export default function MoviePage() {
                     {/* parte destra */}
                     <p>Average: star</p>
                 </div>
-
                 {/* parte inferiore */}
-                <ReviewCard />
-                <ReviewCard />
-                <ReviewCard />
-                <ReviewCard />
-                <ReviewCard />
+                {renderReviews()}
             </section>
 
             <section className="card" id="new-review" >
