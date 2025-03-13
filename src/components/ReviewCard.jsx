1506d74fe7ef6157351452cc203ccf72f49d58f8
GiuntaGiovanni <jhonniest741@gmail.com>
2025-03-13 17:04:55 +0100

    [fix] style

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,17 +1,25 @@
 export default function ReviewCard({ reviewProp }) {
     const { created_at, id, movie_id, name, text, vote } = reviewProp
     return (
-        <div className="card mb-4 bg-black">
+        <div className="card mb-4">
             <div className="card-body">
-                <p className="card-text">
-                    {text}
-                </p>
-                <strong>
-                    Vote:{vote}
-                </strong>
-                <address>
-                    <i>By {name}</i>
-                </address>
+                <div className="row">
+                    <div className="col-6">
+                        <address>
+                            <i>{name}</i>
+                        </address>
+                    </div>
+                    <div className="col-6 rank">
+                        <strong>
+                            Vote:{vote}
+                        </strong>
+                    </div>
+                    <div className="col-12">
+                        <p className="card-text ">
+                            {text}
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     )
